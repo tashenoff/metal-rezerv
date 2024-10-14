b6b318488616245d136d2daa381a3a8f76b85103
tashenoff <kail.dead.kd@gmail.com>
2024-10-14 18:36:27 +0600

    .

--- pages/listings.js
+++ pages/listings.js
@@ -1,8 +1,8 @@
 // pages/listings.js
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Header from '../components/Header';
 import SearchListings from '../components/SearchListings'; // Импортируем компонент поиска
+import Layout from '../components/Layout';
 
 const Listings = () => {
     const [listings, setListings] = useState([]);
@@ -34,32 +34,35 @@ const Listings = () => {
     };
 
     return (
-        <div>
-            <Header />
-            <div className='container mx-auto'>
-                <h1>Объявления</h1>
-                <SearchListings onSearchResults={handleSearchResults} /> {/* Передаём функцию для обработки результатов поиска */}
-                {searchMessage && <p>{searchMessage}</p>} {/* Выводим сообщение о поиске */}
-                {filteredResults.length === 0 ? (
-                    <p>Нет опубликованных объявлений.</p>
-                ) : (
-                    <ul>
-                        {filteredResults.map((listing) => (
-                            <li className='p-5 bg-white border my-5' key={listing.id}>
-                                <Link href={`/listing/${listing.id}`}>
-                                    <p className='font-bold'>{listing.title}</p>
-                                    <p>{listing.content}</p>
-                                    <div className='my-5 border border-t border-opacity-25'>
-                                        <p>Дата публикации: {listing.publishedAt}</p>
-                                        <p>Срок поставки: {listing.deliveryDate}</p>
-                                    </div>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                )}
+
+        <Layout>
+            <div>
+
+                <div className='container mx-auto'>
+                    <h1>Объявления</h1>
+                    <SearchListings onSearchResults={handleSearchResults} /> {/* Передаём функцию для обработки результатов поиска */}
+                    {searchMessage && <p>{searchMessage}</p>} {/* Выводим сообщение о поиске */}
+                    {filteredResults.length === 0 ? (
+                        <p>Нет опубликованных объявлений.</p>
+                    ) : (
+                        <ul>
+                            {filteredResults.map((listing) => (
+                                <li className='p-5 bg-white border my-5' key={listing.id}>
+                                    <Link href={`/listing/${listing.id}`}>
+                                        <p className='font-bold'>{listing.title}</p>
+                                        <p>{listing.content}</p>
+                                        <div className='my-5 border border-t border-opacity-25'>
+                                            <p>Дата публикации: {listing.publishedAt}</p>
+                                            <p>Срок поставки: {listing.deliveryDate}</p>
+                                        </div>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
             </div>
-        </div>
+        </Layout>
     );
 };
 
