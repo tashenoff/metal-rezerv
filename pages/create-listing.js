db3c74b3f0b5cdec26aeb56eb37ba952a66d9db3
tashenoff <kail.dead.kd@gmail.com>
2024-10-31 14:08:21 +0600

    .

--- pages/create-listing.js
+++ pages/create-listing.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 import Notification from '../components/Notification';
 import Input from '../components/Input';
 import FormSelect from '../components/FormSelect';
-import Textarea from '../components/Textarea'; 
+import Textarea from '../components/Textarea';
 
 const CreateListing = () => {
   const [title, setTitle] = useState('');
@@ -15,16 +15,14 @@ const CreateListing = () => {
   const [message, setMessage] = useState('');
   const [messageType, setMessageType] = useState('');
   const [isClient, setIsClient] = useState(false);
-  const [categories, setCategories] = useState([]); // Состояние для категорий
-  const [selectedCategoryId, setSelectedCategoryId] = useState(''); // Состояние для выбранной категории
+  const [categories, setCategories] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState('');
   const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (!token) router.push('/login');
     setIsClient(true);
-
-    // Получаем категории
     fetchCategories();
   }, []);
 
@@ -41,6 +39,19 @@ const CreateListing = () => {
     }
   };
 
+  const escapeHtml = (unsafe) => {
+    return unsafe
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#039;');
+  };
+
+  const stripHtmlTags = (html) => {
+    return html.replace(/<[^>]*>/g, ''); // Удаляет все HTML-теги
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const token = localStorage.getItem('token');
@@ -53,7 +64,14 @@ const CreateListing = () => {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ title, content, deliveryDate, purchaseDate, expirationDate, categoryId: selectedCategoryId }), // Передаем выбранную категорию
+        body: JSON.stringify({
+          title: escapeHtml(title),
+          content: escapeHtml(stripHtmlTags(content)), // Очистка содержимого
+          deliveryDate,
+          purchaseDate,
+          expirationDate,
+          categoryId: selectedCategoryId,
+        }),
       });
 
       if (response.ok) {
@@ -64,7 +82,7 @@ const CreateListing = () => {
         setDeliveryDate('');
         setPurchaseDate('');
         setPublicationPeriod('1d');
-        setSelectedCategoryId(''); 
+        setSelectedCategoryId('');
       } else {
         const errorData = await response.json();
         setMessage(`Ошибка: ${errorData.error}` || 'Ошибка при добавлении объявления.');
@@ -100,57 +118,50 @@ const CreateListing = () => {
 
   return (
     <Layout>
-    
-       
-        {message && <Notification message={message} type={messageType} />}
-        <div className="">
-
-
-          <form className='grid grid-cols-12 gap-4' onSubmit={handleSubmit}>
-            <div className='col-span-8 card bg-base-200 p-5'>
-              <Input label="Заголовок" value={title} onChange={(e) => setTitle(e.target.value)} required />
-              <Textarea
-                id="content"
-                placeholder="Содержимое:"
-                value={content}
-                onChange={setContent} // Используем setContent напрямую
-                required
-              />
-            </div>
-            <div className='col-span-4 card bg-base-200 p-5'>
-              <Input label="Дата доставки" type="date" value={deliveryDate} onChange={(e) => setDeliveryDate(e.target.value)} required />
-              <Input label="Дата закупа" type="date" value={purchaseDate} onChange={(e) => setPurchaseDate(e.target.value)} required />
-              <FormSelect
-                label="Категория"
-                value={selectedCategoryId}
-                onChange={(e) => setSelectedCategoryId(e.target.value)}
-                options={[
-                  { value: '', label: 'Выберите категорию' }, // Пустая опция
-                  ...categories.map(category => ({ value: category.id, label: category.name })), // Преобразуем категории в нужный формат
-                ]}
-                required
-              />
-              <FormSelect
-                label="Период публикации"
-                value={publicationPeriod}
-                onChange={(e) => setPublicationPeriod(e.target.value)}
-                options={[
-                  { value: '5m', label: '5 минут' },
-                  { value: '1d', label: '1 день' },
-                  { value: '2d', label: '2 дня' },
-                  { value: '3d', label: '3 дня' },
-                ]}
-              />
-
-<button type="submit" className="btn btn-primary">
-                Добавить объявление
-              </button>
-              
-            </div>
-           
-          </form>
+      {message && <Notification message={message} type={messageType} />}
+      <div>
+        <form className='grid grid-cols-12 gap-4' onSubmit={handleSubmit}>
+          <div className='col-span-8 card bg-base-200 p-5'>
+            <Input label="Заголовок" value={title} onChange={(e) => setTitle(e.target.value)} required />
+            <Textarea
+              id="content"
+              placeholder="Содержимое:"
+              value={content}
+              onChange={setContent}
+              required
+            />
+          </div>
+          <div className='col-span-4 card bg-base-200 p-5'>
+            <Input label="Дата доставки" type="date" value={deliveryDate} onChange={(e) => setDeliveryDate(e.target.value)} required />
+            <Input label="Дата закупа" type="date" value={purchaseDate} onChange={(e) => setPurchaseDate(e.target.value)} required />
+            <FormSelect
+              label="Категория"
+              value={selectedCategoryId}
+              onChange={(e) => setSelectedCategoryId(e.target.value)}
+              options={[{ value: '', label: 'Выберите категорию' }, ...categories.map(category => ({ value: category.id, label: category.name }))]}
+              required
+            />
+            <FormSelect
+              label="Период публикации"
+              value={publicationPeriod}
+              onChange={(e) => setPublicationPeriod(e.target.value)}
+              options={[
+                { value: '5m', label: '5 минут' },
+                { value: '1d', label: '1 день' },
+                { value: '2d', label: '2 дня' },
+                { value: '3d', label: '3 дня' },
+              ]}
+            />
+            <button type="submit" className="btn btn-primary">
+              Добавить объявление
+            </button>
+          </div>
+        </form>
+        <div className="output mt-5">
+          <h2>Результат:</h2>
+          <div>{stripHtmlTags(content)}</div> {/* Отображение чистого текста без тегов */}
         </div>
-     
+      </div>
     </Layout>
   );
 };
