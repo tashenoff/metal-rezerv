3e656bc8de7201963e70b469b01999a3c43c8e2e
tashenoff <kail.dead.kd@gmail.com>
2025-01-24 14:31:35 +0600

    .

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,20 @@ import store from '../store/store';
 import { AuthProvider } from '../contexts/AuthContext';
 import '../utils/i18n'; // Инициализация i18next
 import { appWithTranslation } from 'next-i18next';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <AuthProvider>
-        <Component {...pageProps} data-theme="light" />
-      </AuthProvider>
-    </Provider>
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </Head>    <Provider store={store}>
+        <AuthProvider>
+          <Component {...pageProps} data-theme="light" />
+        </AuthProvider>
+      </Provider>
+    </>
+
   );
 }
 
